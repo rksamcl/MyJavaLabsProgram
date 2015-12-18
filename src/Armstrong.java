7bbd5b0ed39bb18dc3a5b6c978a6437d31bc551d
rksamcl <rksamcl@gmail.com>
2015-12-18 21:00:56 +0530

    add hello in print statement

--- src/Armstrong.java
+++ src/Armstrong.java
@@ -2,7 +2,7 @@ package com.amar;
 import java.util.Scanner;
 public class Armstrong {
 public static void main(String[] args) {
-		System.out.println("Enter the any Integer value--");
+		System.out.println("Enter the any Integer value--hello");
 		Scanner nm=new Scanner(System.in);
 		int n=nm.nextInt();
 		int res,sum=0;
