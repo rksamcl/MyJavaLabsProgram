1789587056752cec4539fc2461de40acbab3da23
rksamcl <rksamcl@gmail.com>
2015-12-18 19:21:45 +0530

    completed armstrong module

---
+++ src/Armstrong.java
@@ -0,0 +1,25 @@
+package com.amar;
+import java.util.Scanner;
+public class Armstrong {
+public static void main(String[] args) {
+		System.out.println("Enter any Integer value--");
+		Scanner nm=new Scanner(System.in);
+		int n=nm.nextInt();
+		int res,sum=0;
+	    int tem=n;
+	    while(n>0)
+	     {
+	       res=n%10;
+	       sum=sum+res*res*res;
+	       n=n/10;
+	     }
+	      if(sum==tem)
+	       {
+	       System.out.println(tem+" is Armstrong");
+	       }
+	      else
+	       { 
+		     System.out.println(tem+" is Not Armstrong");
+	       }
+	    }
+     }
