5fcb28191168b94fc3f1d8303af30961453bdb2d
balindra <balindrakm@gmail.com>
2015-12-18 19:43:34 +0530

    completed string progam

---
+++ src/StringPelindrom.java
@@ -0,0 +1,45 @@
+package com.amar;
+import java.util.*;
+
+class Find{
+	String str,revstr="";
+	int i;
+	Find()
+	    {
+		System.out.println("Enter you String value");
+		Scanner sc=new Scanner(System.in);
+		str=sc.nextLine();
+		}
+		
+	void revString()
+	{
+		int len_str= str.length();
+		for(i=len_str-1;i>=0;i--)
+		{
+			revstr=revstr+str.charAt(i);
+		}
+	}
+	
+	void display()
+	{
+		
+		if(str.equals(revstr))
+		   
+			System.out.println(revstr+" is Pelindrom");
+		    
+		else
+			System.out.println(revstr+" is not Pelindrom");
+		
+	}
+		
+}
+		
+class StringPelindrom {	
+		
+	public static void main(String[] args) {
+		Find f=new Find();
+		f.revString();
+		f.display();
+	}
+
+}
